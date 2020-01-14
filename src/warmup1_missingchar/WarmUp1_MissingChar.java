12382489ef5295f6882c829e679fbbcaf7bedfd2
Chris Papadopoulos <xristos8170@gmail.com>
2020-01-14 14:22:25 +0200

    Add files via upload

---
+++ src/warmup1_missingchar/WarmUp1_MissingChar.java
@@ -0,0 +1,16 @@
+package warmup1_missingchar;
+
+public class WarmUp1_MissingChar {
+
+    public static void main(String[] args) {
+        System.out.println(missingChar("kitten", 1));
+        System.out.println(missingChar("kitten", 0));
+        System.out.println(missingChar("kitten", 4));
+    }
+    
+    public static String missingChar (String str, int n){
+        
+        return str.substring(0,n) + str.substring(n+1,str.length());
+    
+}
+}
